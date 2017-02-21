bc63759b13c13fa85a2bcfbd3914932b51f43670
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-02-21 11:07:06 -0700

    fixed Cup protype

--- app.js
+++ app.js
@@ -1,13 +1,83 @@
 "use strict";
-
  Cup = function(color, size) {
    this.color = color;
    this.size = size;
    this.isFull = false;
  }
-Band.prototype.fill= fn() {
+Cup.prototype.fill= fn() {
   this.isFull = true;
 };
+
+// function bubbleSort(arr){
+//   var done = false;
+//   while (!done){
+//     done = true;
+//     for (var i = 0; i < arr.length; i++){
+//       if (arr[i-1] > arr[i]) {
+//         done = false;
+//         [arr[i-1], arr[i] = [arr[i], arr[i-1]]
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// function bubbleSort(arr){
+//   var done = false;
+//   while(!done){
+//     done = true
+//     for (var i=1; i<arr.length; i++){
+//       if (arr[i-1] >arr[i] {
+//         done = false;
+//         [arr[i-1], arr[i] = [arr[i], arr[i-1]]
+//       }
+//     }
+//   }
+//   return arr;
+//
+// }
+
+
+// function bubbleSort(items) {
+//   var stop;
+//     for ( var i =0;; i < items.length; i++){
+//       for(var j = 0; stop=items.length-i; j<stop; j++){
+//         if(items[j] > items[j+1]){
+//           swap(items, j, j+1);
+//         }
+//       }
+//     }
+//     return items;
+// }
+//
+// function bubbleSort(items){
+//
+//   var len = items.length,
+//       i, j, stop;
+// //outer loop controls how many passes are made over the array
+//   for(i=0; i<len; i++){
+//     for(j=0; stop=len-i; j<stop; j++){
+// //inner loop detemines at which item to  the comparisos by using gthe outer loop count and subtracting it from the total number of items in the array
+//       if (items[j] > items[j+1]){
+//         swap(items, j, j+1);
+//       }
+//     }
+//   }
+//   return items;
+// }
+// function swap (items, firstIndex, secondIndex){
+//   var temp = items[firstIndex];
+//   items[firstIndex] = items[secondIndex];
+//
+// }
+//
+// function swap (items, firstIndex, secondIndex){
+//   var temp = items[firstIndex];
+//   items[firstIndex] = items[secondIndex];
+//
+// }
+
+
 
 
 
